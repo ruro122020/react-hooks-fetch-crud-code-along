d03973bc99ae5d8eb0e41553342a7eb475ea8e40
Ruth <ruro122020@gmail.com>
2023-10-23 16:19:51 -0400

    create 'GET' fetch request for items

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,6 +7,12 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  //Fetch Request
+  useEffect(()=>{
+    fetch('http://localhost:4000/items')
+    .then(res => res.json())
+    .then(items => setItems(items))
+  },[])
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
